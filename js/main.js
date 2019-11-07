101c10fccb0f59ba13d71634cdbe4d5ab064dc24
august.moller <ag.man@live.se>
2019-11-07 11:05:47 +0100

    changed logo and added a game

--- js/main.js
+++ js/main.js
@@ -22,25 +22,36 @@ wrap.style.background = "white";
 logo.style.fill = "black"; */
 
 function modalFunction1() {
+    modalTitle.innerHTML = "Pumpkin snake";
+    modalText.innerHTML = "Snake but you play as a pumpkin who collects candy to grow.";
+    modalLink.innerHTML = "Play the game here!";
+    image.src = "images/pumpkinsnake.png"
+    image.alt = "Space Invaders"
+    modalLink.href = "https://snake.agman.xyz"
+
+}
+
+function modalFunction2() {
     modalTitle.innerHTML = "Space Invaders";
-    modalText.innerHTML = "En re-creation av det populära arcad spelet Space Invaders.";
-    modalLink.innerHTML = "Länk till GitHub repot.";
+    modalText.innerHTML = "A re-creation of the popular arcade game Space Invaders.";
+    modalLink.innerHTML = "Link to the github repo.";
     image.src = "images/spaceinvaders.png"
     image.alt = "Space Invaders"
     modalLink.href = "https://github.com/Agman10/Space-InvadersFinal"
-    
+
 }
-function modalFunction2() {
+
+function modalFunction3() {
     modalTitle.innerHTML = "VG Currency Clicker";
-    modalText.innerHTML = "Ett clicker spel som använder spel valuta klicka på frågetecken blocket för att få coins och köp andra spel valutor för att öka antalet coins som du känar.";
-    modalLink.innerHTML = "Testa spelet här!";
+    modalText.innerHTML = "A clicker game that uses video game currencies klick on the question-mark block to get coins and buy other game currencies to increase the ammount of coins you earn.";
+    modalLink.innerHTML = "Play the game here!";
     image.src = "images/clicker.png"
     image.alt = "VG Currency Clicker"
     modalLink.href = "https://agman10.github.io/clicker/"
 }
 
 //When you click the logo dark mode turns on and off, changes the colors to dark and bright
-logo.addEventListener("click", function(){
+logo.addEventListener("click", function () {
     darkMode = !darkMode //darkmode switches from false to true
     console.log("darkMode: " + darkMode);
 
@@ -50,46 +61,46 @@ logo.addEventListener("click", function(){
     wrap.style.background = darkMode ? "#232323" : "white";
     logo.style.fill = darkMode ? "white" : "black";
     modalBackground.style.background = darkMode ? "#232323" : "white";
-    modalClose.style.color  = darkMode ? "rgb(240, 80, 80)" : "rgb(210, 50, 50)";
+    modalClose.style.color = darkMode ? "rgb(240, 80, 80)" : "rgb(210, 50, 50)";
     logoX.style.fill = darkMode ? "lightcoral" : "red";
     logoY.style.fill = darkMode ? "lightgreen" : "green";
     logoZ.style.fill = darkMode ? "lightblue" : "blue";
-    
-    
+
+
 });
 /* function Theme() {
 
     wrap.style.background = "white" ? "black" : "white"; */
 
-    /* if (document.body.style.background === "#bbbbbb") {
-        document.body.style.background = "#111111";
-    } else {
-        document.body.style.background = "#bbbbbb";
-    }
+/* if (document.body.style.background === "#bbbbbb") {
+    document.body.style.background = "#111111";
+} else {
+    document.body.style.background = "#bbbbbb";
+}
 
-    if (wrap.style.background === "white") {
-        wrap.style.background = "#232323";
-    } else {
-        wrap.style.background = "white";
-    }
+if (wrap.style.background === "white") {
+    wrap.style.background = "#232323";
+} else {
+    wrap.style.background = "white";
+}
 
-    if (logo.style.fill === "black") {
-        logo.style.fill = "white";
-    } else {
-        logo.style.fill = "black";
-    } */
+if (logo.style.fill === "black") {
+    logo.style.fill = "white";
+} else {
+    logo.style.fill = "black";
+} */
 
 
-    
-    //console.log("hello")
 
-    //console.log(darkMode ? "LightMode" : "DarkMode");
+//console.log("hello")
 
-    /* logo.style.color = "black" ? "white" : "white"; */
-    /* document.body.style.background = darkMode ? "white" : "black";
-    document.body.style.background ="#000000";
-    document.getElementById("wrap").style.background = "#232323"; */
-    
+//console.log(darkMode ? "LightMode" : "DarkMode");
+
+/* logo.style.color = "black" ? "white" : "white"; */
+/* document.body.style.background = darkMode ? "white" : "black";
+document.body.style.background ="#000000";
+document.getElementById("wrap").style.background = "#232323"; */
+
 /* } */
 
 
