b84b1d4b0a0d2aa3a27dbc093b77ef3d2fccf6e7
August Möller <ag.man@live.se>
2021-12-07 22:33:15 +0100

    Updated Info

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ logo.style.fill = "black"; */
 
 function modalFunction1() {
     modalTitle.innerHTML = "Pumpkin snake";
-    modalText.innerHTML = "Snake but you play as a pumpkin who collects candy to grow.";
+    modalText.innerHTML = "Snake but you play as a pumpkin who collects candy to grow. This was a halloween school assignment";
     modalLink.innerHTML = "Play the game here!";
     modalLink2.innerHTML = "Link to the github repo.";
     image.src = "images/pumpkinsnake.png"
@@ -35,7 +35,7 @@ function modalFunction1() {
 
 function modalFunction2() {
     modalTitle.innerHTML = "Space Invaders JS";
-    modalText.innerHTML = "A re-creation of the popular arcade game Space Invaders made in javascript, not finished yet.";
+    modalText.innerHTML = "A re-creation of the popular arcade game Space Invaders made in javascript.";
     modalLink.innerHTML = "Play the game here!";
     modalLink2.innerHTML = "Link to the github repo.";
     image.src = "images/spaceinvadersJS.png"
@@ -45,7 +45,7 @@ function modalFunction2() {
 }
 function modalFunction3() {
     modalTitle.innerHTML = "Litterary War";
-    modalText.innerHTML = "A space shooter that uses text as graphich. Not finished yet.";
+    modalText.innerHTML = "A space shooter that uses text as graphich. This was a school assignemnt.";
     modalLink.innerHTML = "Play the game here!";
     modalLink2.innerHTML = "Link to the github repo.";
     image.src = "images/litterarywarBeta.png"
@@ -79,34 +79,44 @@ function modalFunction5() {
 
 function modalFunction6() {
     modalTitle.innerHTML = "Unity platformer";
-    modalText.innerHTML = "A work in progress platformer game made in unity";
-    modalLink.innerHTML = "Link to the github repo.";
-    modalLink2.innerHTML = "";
+    modalText.innerHTML = "A work in progress platformer game made in unity. Sprites made by me" + "<br /> <br />" + "this was one of my first unity projects so the code on the github is very spaghetti but i have a newer version that i haven't added to github yet where i rewrote the code and using a newer version of unity.";
+    modalLink.innerHTML = "Link to the old github repo.";
+    modalLink2.innerHTML = "Donwload the prototype";
     image.src = "images/unityplatformer.png"
     image.alt = "Unity platformer"
     modalLink.href = "https://github.com/Agman10/unity-platformer"
-    modalLink2.href = ""
+    modalLink2.href = "https://drive.google.com/file/d/1mx1vbJKXoV8k-gUI4bxV-CjR6Q6w5MXY/view?usp=sharing"
 }
 
 function modalFunction7() {
-    modalTitle.innerHTML = "Ball Game";
-    modalText.innerHTML = "A platformer where you play as a ball and you can't jump, you unlock levels by collecting the star en enter the exit portal. If you destroy all boxes in a level you get a gem. The game is based on the ball levels in Crash the Wrath of Cortex.";
+    modalTitle.innerHTML = "Titled Ball Game 1";
+    modalText.innerHTML = "A platformer where you play as a ball and you can't jump, you unlock levels by collecting the star en enter the exit portal. If you destroy all boxes in a level you get a gem. The game is based on the ball levels in Crash the Wrath of Cortex." + "<br /> <br />" + "you can play the game in the GameBuild folder in github";
     modalLink.innerHTML = "Link to the github repo + download.";
     modalLink2.innerHTML = "";
     image.src = "images/BallGame2.png"
-    image.alt = "Unity platformer"
+    image.alt = "Ball Game"
     modalLink.href = "https://github.com/Agman10/BallGamePlatformer"
     modalLink2.href = ""
 }
 
 function modalFunction8() {
     modalTitle.innerHTML = "Clönk";
-    modalText.innerHTML = "A Rougelike game where you choose powerups and the boss get the ones you didn't pick";
+    modalText.innerHTML = "A Rougelike game where you choose powerups and the boss get the ones you didn't pick." + "<br /> <br />" + "I work mainly on the AI, traps , new projectiles and the new ability system and other...";
     modalLink.innerHTML = "Download the game here";
     modalLink2.innerHTML = "";
     image.src = "images/Clönk_1.png"
-    image.alt = "Unity platformer"
+    image.alt = "Clönk"
     modalLink.href = "https://iron-evil.itch.io/clonk"
+    modalLink2.href = ""
+}
+function modalFunction9() {
+    modalTitle.innerHTML = "Quantum Arena";
+    modalText.innerHTML = "An online multiplayer game where you play as a tank. The goal of the game is to have the most points by damaging or killing enemies, killing your teammates will reduce your points.In some maps, holding the control point in the middle of the map also awards points. There is also a FFA gamemode" + "<br /> <br />" + "This is a group project that i was assigned to I did the Tank AI, UI programming, score systemn the controls and some online stuff, and other stuff";
+    modalLink.innerHTML = "Download the Game";
+    modalLink2.innerHTML = "";
+    image.src = "images/QuantumArena_2.png"
+    image.alt = "Quantum Arena"
+    modalLink.href = "https://drive.google.com/file/d/1lP-EXODsm34D7w7LCjQnq5EVwldezLl2/view?usp=sharing"
     modalLink2.href = ""
 }
 
